ef2d45b872636236518d30560ab7efb1f01fa516
Daniel Lozano <dlozanoc862@gmail.com>
2023-03-10 19:01:38 -0500

    :tada: Initial Project: TodoList in Console
    [ADD] Menu in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+require('colors');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
+
+console.clear();
+
+const main = async() => {
+    console.log('Hola Mundo');
+    
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        console.log({opt});
+
+        await pausa();
+
+    } while (opt !== '0');
+    
+    //pausa();
+}
+
+main();
